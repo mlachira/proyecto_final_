34998c3bd9ef74ba9d993dae6c5c5f12cf3ee5ca
mlachira <119971234+mlachira@users.noreply.github.com>
2023-01-03 12:18:35 -0600

    borrador dieta alimentaria

---
+++ Dieta alimentaria.R
@@ -0,0 +1,9 @@
+#Perder grasa
+sexo
+fecha_de_nacimiento 
+estatura 
+peso_actual
+ejercicio
+peso_objetivo
+#Mantener peso 
+#Ganar musculo
--- Objetivo.R
+++ Objetivo.R
@@ -1,4 +1,15 @@
-#Perder grasa
+#Para calcular las kcal, proteínas, carbohidratos y grasas que se consumen diario se le pide que llene lo que se le pide. 
+
+dieta_alimentaria<- function(){
+  desayuno<-readline(prompt = "Ingresa los alimentos que consumiste en el desayuno del lunes")
+  colación1<-readline(prompt = "Ingresa los alimentos que consumiste en la colación del lunes")
+  comida<-readline(prompt = "Ingresa los alimentos que consumiste en la comida del lunes")
+  colación2<- readline(prompt = "Ingresa los alimentos que consumiste en la colación del lunes")
+  cena<- readline(prompt = "Ingresa los alimentos que consumiste en la cena del lunes")
+  suma_kcal
+  return(print(paste("Las kcal que consumiste el lunes son", suma_kcal, "en proteínas consumiste", suma_proteina, "en carbohidratos consumiste", suma_carbohidratos, "en grasas consimiste", suma_grasas)))
+}
+dieta_alimentaria()
 sexo
 fecha_de_nacimiento 
 estatura 
