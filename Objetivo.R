5907ba2f6891f23a0645275de5c1e45a016b538c
mlachira <119971234+mlachira@users.noreply.github.com>
2023-01-03 19:48:15 -0600

    Borrador calcular kcal

--- Objetivo.R
+++ Objetivo.R
@@ -1,20 +1,10 @@
 #Para calcular las kcal, proteínas, carbohidratos y grasas que se consumen diario se le pide que llene lo que se le pide. 
-
-dieta_alimentaria<- function(){
-  desayuno<-readline(prompt = "Ingresa los alimentos que consumiste en el desayuno del lunes")
-  colación1<-readline(prompt = "Ingresa los alimentos que consumiste en la colación del lunes")
-  comida<-readline(prompt = "Ingresa los alimentos que consumiste en la comida del lunes")
-  colación2<- readline(prompt = "Ingresa los alimentos que consumiste en la colación del lunes")
-  cena<- readline(prompt = "Ingresa los alimentos que consumiste en la cena del lunes")
-  suma_kcal
-  return(print(paste("Las kcal que consumiste el lunes son", suma_kcal, "en proteínas consumiste", suma_proteina, "en carbohidratos consumiste", suma_carbohidratos, "en grasas consimiste", suma_grasas)))
+alimento_origen_animal
+dieta_origen_animal<- function(){
+  alimento_dieta_oa<-readline(prompt = "Ingresa alimento de origen animal")
+  kcal_alimento_dieta_oa<- alimento_origen_animal[alimento_dieta_oa, "kcal"]
+  as.numeric(kcal_alimento_dieta_oa)
+  return(print(paste("Las kcal son", kcal_alimento_dieta_oa)))
 }
-dieta_alimentaria()
-sexo
-fecha_de_nacimiento 
-estatura 
-peso_actual
-ejercicio
-peso_objetivo
-#Mantener peso 
-#Ganar musculo
+dieta_origen_animal()
+Pescado
