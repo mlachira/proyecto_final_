6b8a185c47cb2c836836ff3e162a430f8915694f
KarinaRan <120007431+KarinaRan@users.noreply.github.com>
2023-01-03 20:59:53 -0600

    Corrección completa de acentos

--- Objetivo.R
+++ Objetivo.R
@@ -1,10 +1,10 @@
-#Para calcular las kcal, proteínas, carbohidratos y grasas que se consumen diario se le pide que llene lo que se le pide. 
-alimento_origen_animal
-dieta_origen_animal<- function(){
-  alimento_dieta_oa<-readline(prompt = "Ingresa alimento de origen animal")
-  kcal_alimento_dieta_oa<- alimento_origen_animal[alimento_dieta_oa, "kcal"]
-  as.numeric(kcal_alimento_dieta_oa)
-  return(print(paste("Las kcal son", kcal_alimento_dieta_oa)))
-}
-dieta_origen_animal()
-Pescado
+#Perder grasa
+sexo
+fecha_de_nacimiento 
+estatura 
+peso_actual
+ejercicio
+peso_objetivo
+#Mantener peso 
+#Ganar musculo
+
